a5d83d2f2593b731478c62564ba9fb865c666ea8
Matthew Duong <matt@timeweave.com.au>
2016-04-30 19:01:05 +1000

    Added stub paths

--- site/api/index.js
+++ site/api/index.js
@@ -27,6 +27,56 @@ app.put('/upload', upload.single('picture_file'), function(req, res) {
     return;
 });
 
+
+function attachListeners(ls,res){
+	let hasErrored=false;
+
+	ls.stdout.on('data', function (data) {
+		let out={}
+		//reset flag
+		hasErrored=false;
+		console.log('stdout: ' + data);
+		data=JSON.parse(data);
+
+		if(data.error){
+			hasErrored=true;
+			out['error']=data.error;
+			res.json(out);
+			return;
+		}
+
+		out=data;
+		ctx.emit('import',out);
+	});
+
+	ls.stderr.on('data', function (data) {
+		let out={};
+		console.log('stderr: ' + data);
+		//slack notify
+		slack_error(data);
+		hasErrored=true;
+		out['error']='Internal server error.';
+		res.json(out);
+	});
+
+	ls.on('exit', function (code) {
+		let out={}
+		console.log('child process exited with code ' + code);
+		if(code!=0){
+			out['error']='Internal server error.';
+			res.json(out);
+			return;
+		}
+		//no output needed
+		if(hasErrored){
+			return;
+		}
+
+		out['complete']='Import was successful!';
+		res.json(out);
+		return;
+	});
+}
 app.listen(config.app.port);
 
 console.log('\n\n\n\n**********STARTING EXPRESS SERVER ON PORT '+config.app.port+'\n\n\n\n');
